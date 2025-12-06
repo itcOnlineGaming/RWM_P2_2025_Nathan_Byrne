b4940649e62c77981fcd38689bf6adfaba7caab8
Nathan-Byrne <c00281281@setu.ie>
2025-12-06 15:58:04 +0000

    #14 Updated Exercise Tooltips

--- Yoga/src/lib/components/SelectExercisesArea.svelte
+++ Yoga/src/lib/components/SelectExercisesArea.svelte
@@ -54,23 +54,33 @@ Possible Choices:
 
 <div class="button-wrapper">
     <button class="btn" class:green-border={$exerciseEnabled[0]} class:red-border={!$exerciseEnabled[0]} on:click={() => updateToggle(0)}>🧘
-        <div class="tooltip">Meditate</div> </button>
+        <div class="tooltip">Meditate<br>Relax your Body, and Focus<br>
+            Difficulty: Easy</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[1]} class:red-border={!$exerciseEnabled[1]} on:click={() => updateToggle(1)}>😮‍💨
-        <div class="tooltip">Deep Breaths</div> </button>
+        <div class="tooltip">Deep Breaths<br>Inhale Slowly and Exhale Gently<br>
+            Difficulty: Easy</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[2]} class:red-border={!$exerciseEnabled[2]} on:click={() => updateToggle(2)}>🤸
-        <div class="tooltip">Cartwheel</div> </button>
+        <div class="tooltip">Cartwheel<br>Rotate your Body with a Sideways Movement<br>
+            Difficulty: Hard</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[3]} class:red-border={!$exerciseEnabled[3]} on:click={() => updateToggle(3)}>🙆
-        <div class="tooltip">Loosen Up</div> </button>
+        <div class="tooltip">Loosen Up<br>Light Movements to Reduce Tension<br>
+            Difficulty: Easy</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[4]} class:red-border={!$exerciseEnabled[4]} on:click={() => updateToggle(4)}>🙇⬆️
-        <div class="tooltip">Downward Dog</div> </button>
+        <div class="tooltip">Downward Dog<br>Make a Interted V-Shape with your Body<br>
+            Difficulty: Medium</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[5]} class:red-border={!$exerciseEnabled[5]} on:click={() => updateToggle(5)}>🌉
-        <div class="tooltip">Bridge Pose</div> </button>
+        <div class="tooltip">Bridge Pose<br>Lie Back, Bend Knees and Lift Hips Upwards<br>
+            Difficulty: Hard</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[6]} class:red-border={!$exerciseEnabled[6]} on:click={() => updateToggle(6)}>🧍🌿
-        <div class="tooltip">Tree Pose</div> </button>
+        <div class="tooltip">Tree Pose<br>Stand with One Leg and Balance<br>
+            Difficulty: Easy</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[7]} class:red-border={!$exerciseEnabled[7]} on:click={() => updateToggle(7)}>🧍➖
-        <div class="tooltip">Plank</div> </button>
+        <div class="tooltip">Plank<br>Maintain a Straight Formation with your Body<br>
+            Difficulty: Hard</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[8]} class:red-border={!$exerciseEnabled[8]} on:click={() => updateToggle(8)}>🚶‍♂️✋
-        <div class="tooltip">Warrior I</div> </button>
+        <div class="tooltip">Warrior I<br>Back Foot Lunge with Raised Overhead Arms<br>
+            Difficulty: Medium</div> </button>
     <button class="btn" class:green-border={$exerciseEnabled[9]} class:red-border={!$exerciseEnabled[9]} on:click={() => updateToggle(9)}>🚶‍♂️👈👉
-        <div class="tooltip">Warrior II</div> </button>
+        <div class="tooltip">Warrior II<br>Wide-Leg Lunge with Extended Parallel Arms<br>
+            Difficulty: Medium</div> </button>
 </div>
